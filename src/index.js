59f1383d88c070913ea4bba6aae1b2b72da018c1
imLaeL <isaque.lael@academico.ifpb.edu.br>
2023-04-15 21:38:39 -0300

    preciso concertar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import { clinicas } from './data/clinics.js';
-
+import path from 'path';
 
 class HTTPError extends Error {
     constructor(message, code) {
@@ -16,7 +16,13 @@ server.use(morgan('tiny'));
 
 server.use(express.json());
 
-server.use(express.static('public'));
+server.use(express.static('/public'));
+
+// Mostra a página inicial
+
+server.get('/', (req, res) => {
+    res.sendFile('../public/index.html'), { root: __dirname};
+});
 
 // Mostra o arquivo json contendo as clínicas de joão pessoa 
 
@@ -32,15 +38,16 @@ server.use((req, res, next) => {
 
 // Error handler
 
-server.use((err, req, res, next) => {
+
+//server.use((err, req, res, next) => {
     // console.error(err.stack);
 
-    if (err instanceof HTTPError) {
-        res.status(err.code).json({ message: err.message });
-    } else {
-        res.status(500).json({ message: 'Something broke!' });
-    }
-});
+  //  if (err instanceof HTTPError) {
+    //    res.status(err.code).json({ message: err.message });
+  //  } else {
+    //    res.status(500).json({ message: 'Something broke!' });
+    //}
+//});
 
 server.listen(3000, () => {
     console.log('Servidor está rodando na porta 3000');
