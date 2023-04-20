b3dc36a12474e29615a9810b8871deac7b11648c
imLaeL <isaque.lael@academico.ifpb.edu.br>
2023-04-20 19:54:54 -0300

    cuidados-modificado

--- public/cuidados/js/main.js
+++ public/cuidados/js/main.js
@@ -1,10 +1,12 @@
-const article_gatos = document.querySelector('#gatos-filhotes-texto');
+var article_gatos = document.getElementById("gatos-filhotes-texto");
 
-function showMore() {
-    if (article_gatos.className == "open") {
-        article_gatos.className = "";
-
-    } else {
-        article_gatos.className == "open";        
+function showMore(id) {
+    var item = document.getElementById(id)
+    console.log(item.classList[0].toString());
+    if (item.classList[0].toString() === "close"){
+        item.classList.replace("close", "open");
+    
+    } else if (item.classList[0].toString() === "open") {
+        item.classList.replace("open", "close");
     }
 }
