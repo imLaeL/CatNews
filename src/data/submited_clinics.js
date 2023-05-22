0fb1e0e8875e89d83c23366f1edffe865386d90b
imLaeL <isaque.lael@academico.ifpb.edu.br>
2023-05-22 07:58:57 -0300

    brmodelo

--- src/data/submited_clinics.js
+++
@@ -1,7 +0,0 @@
-export const submited_clinics = [
-    {
-        "name": "clínica",
-        "imageurl": "https://clicandoeandando.com/wp-content/uploads/2016/06/Como-tirar-fotos-melhores-com-qualquer-cmera-golden-hour-800x533.jpg",
-        "city": "João Pessoa"
-    }
-]
