bfbab652982e6ca863a11aee02df4b3f186ae3a6
imLaeL <isaque.lael@academico.ifpb.edu.br>
2023-09-18 09:54:56 -0300

    validação-front-end

--- public/login/js/signup.js
+++ public/login/js/signup.js
@@ -5,13 +5,9 @@ const form = document.querySelector('form');
 async function handleSubmit(event) {
   event.preventDefault();
 
-  const user = Object.fromEntries(new FormData(form));
-  if (
-    user.username &&
-    user.email &&
-    user.password &&
-    user.confirmationPassword
-  ) {
+  if (form.checkValidity()) {
+    const user = Object.fromEntries(new FormData(form));
+  
     const config = {
       method: 'post',
       body: JSON.stringify(user),
@@ -27,5 +23,28 @@ async function handleSubmit(event) {
     } else {
       console.log('Erro no cadastro');
     }
+  } else {
+    form.classList.add('was-validated');
   }
 }
+
+
+form.confirmationPassword.oninput = () => {
+  const password = form.password.value;
+
+  const confirmationPassword = form.confirmationPassword.value;
+
+  if (password !== confirmationPassword) {
+    const error = 'As senhas não são iguais.';
+
+    const confirmationPasswordError = document.querySelector(
+      '#confirmationPassword + .invalid-feedback'
+    );
+
+    confirmationPasswordError.textContent = error;
+
+    form.confirmationPassword.setCustomValidity(error);
+  } else {
+    form.confirmationPassword.setCustomValidity('');
+  }
+};
